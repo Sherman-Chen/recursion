4df4ce69266ffb33de2a1074bb30e926907f9036
Sherman <shc410@gmail.com>
2016-09-01 03:15:31 -0700

    Implement stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,42 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  
+  if (typeof obj === 'string') {
+  	return '"' + obj + '"';
+  }
+  if (typeof obj === 'number') {
+  	return '' + obj;
+  }
+  if (typeof obj === 'boolean') {
+  	return '' + obj;
+  }
+  if (typeof obj === 'undefined') {
+  	return null;
+  }
+  if (typeof obj === 'function') {
+  	return null;
+  }
+  if (obj === null) {
+  	return 'null';
+  }
+
+  if (Array.isArray(obj)) {
+  	var stringifyArray = [];
+  	for (var i = 0; i < obj.length; i++) {
+  		stringifyArray.push(stringifyJSON(obj[i]));
+  	}
+  	return '[' + stringifyArray.join(',') + ']';
+  }
+
+  if (typeof obj === 'object') {
+    var stringObj = [];
+    for (var key in obj) {
+    	var value = stringifyJSON(obj[key]);
+    	if (value !== null)
+      stringObj.push(stringifyJSON(key) + ':' + value);
+    }
+    return '{' + stringObj.join(',') + '}';
+  }
+
 };
