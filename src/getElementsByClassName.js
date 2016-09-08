1d708fc0b48217a74505270f50eaa5fb10557a76
Sherman <shc410@gmail.com>
2016-09-07 21:55:54 -0700

    Refactor code

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,28 +4,45 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-  var results = [];
+// var getElementsByClassName = function(className
+// ) {
+//   // your code here
+//   var results = [];
 
-  var search = function(node) {
-  	if (typeof node.classList !== 'undefined') {
-  		if (node.classList.contains(className)) {
-  			results.push(node);
-  		}
-  	}
+//   var search = function(node) {
+//   	if (typeof node.classList !== 'undefined') {
+//   		if (node.classList.contains(className)) {
+//   			results.push(node);
+//   		}
+//   	}
 
-  	if (document.body.hasChildNodes()) {
-  		for (var element in node.childNodes) {
-  			search(node.childNodes[element]);
-  		}
-  	}
-  };
+//   	if (document.body.hasChildNodes()) {
+//   		for (var element in node.childNodes) {
+//   			search(node.childNodes[element]);
+//   		}
+//   	}
+//   };
 
-  search(document.body);
-  return results;
+//   search(document.body);
+//   return results;
 
-};
+// };
+
+var getElementsByClassName = function(className, node) {
+
+  var nodes = [];
+  node = node || document.body;
+
+  var parts = node.className.split(' ');
+  if (parts.indexOf(className) >= 0) {
+    nodes.push(node);
+  }
+
+  for (var i = 0; i < node.children.length; i++) {
+    var childResults = getElementsByClassName(className, node.children[i]);
+    nodes = nodes.concat(childResults);
+  }
+  return nodes;
+}
 
 
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,44 +3,73 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+// var stringifyJSON = function(obj) {
+//   // your code goes here
   
-  if (typeof obj === 'string') {
-  	return '"' + obj + '"';
-  }
-  if (typeof obj === 'number') {
-  	return '' + obj;
-  }
-  if (typeof obj === 'boolean') {
-  	return '' + obj;
-  }
-  if (typeof obj === 'undefined') {
-  	return null;
-  }
-  if (typeof obj === 'function') {
-  	return null;
-  }
-  if (obj === null) {
-  	return 'null';
-  }
+//   if (typeof obj === 'string') {
+//   	return '"' + obj + '"';
+//   }
+//   if (typeof obj === 'number') {
+//   	return '' + obj;
+//   }
+//   if (typeof obj === 'boolean') {
+//   	return '' + obj;
+//   }
+//   if (typeof obj === 'undefined') {
+//   	return null;
+//   }
+//   if (typeof obj === 'function') {
+//   	return null;
+//   }
+//   if (obj === null) {
+//   	return 'null';
+//   }
 
+//   if (Array.isArray(obj)) {
+//   	var stringifyArray = [];
+//   	for (var i = 0; i < obj.length; i++) {
+//   		stringifyArray.push(stringifyJSON(obj[i]));
+//   	}
+//   	return '[' + stringifyArray.join(',') + ']';
+//   }
+
+//   if (typeof obj === 'object') {
+//     var stringObj = [];
+//     for (var key in obj) {
+//     	var value = stringifyJSON(obj[key]);
+//     	if (value !== null)
+//       stringObj.push(stringifyJSON(key) + ':' + value);
+//     }
+//     return '{' + stringObj.join(',') + '}';
+//   }
+
+// };
+
+var stringifyJSON = function(obj) {
   if (Array.isArray(obj)) {
-  	var stringifyArray = [];
-  	for (var i = 0; i < obj.length; i++) {
-  		stringifyArray.push(stringifyJSON(obj[i]));
-  	}
-  	return '[' + stringifyArray.join(',') + ']';
-  }
-
-  if (typeof obj === 'object') {
-    var stringObj = [];
-    for (var key in obj) {
-    	var value = stringifyJSON(obj[key]);
-    	if (value !== null)
-      stringObj.push(stringifyJSON(key) + ':' + value);
+    var results = [];
+    for (var i =0; i < obj.length; i++) {
+      results.push(stringifyJSON(obj[i]));
     }
-    return '{' + stringObj.join(',') + '}';
+    return '['+results.join(',')+']';
   }
-
+  if (obj && typeof obj === 'object') {
+    var results = [];
+    for (var key in obj) {
+      if (obj[key] === undefined || typeof obj[key] === 'function') {
+        continue;
+      }
+      results.push(stringifyJSON(key)+':'+stringifyJSON(obj[key]));
+    }
+    return '{'+results.join(',')+'}';
+  }
+  if (typeof obj === 'string') {
+    return '"'+obj+'"';
+  }
+  return ''+obj;
 };
+
+
+
+
+
